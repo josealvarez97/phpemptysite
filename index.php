c9c40c7241ed80c4e9bfa8b5ed977007e4f22d3b
José Alvarez <cachorritoalfa2013@gmail.com>
2017-05-29 22:56:34 -0600

    Update index.php

--- index.php
+++ index.php
@@ -98,6 +98,29 @@ $herbie = new Car();
 // show object properties
 echo $herbie->model;
 
+
+
+/*
+
+Null is a special data type which can have only one value: NULL.
+
+A variable of data type NULL is a variable that has no value assigned to it.
+
+Tip: If a variable is created without a value, it is automatically assigned a value of NULL.
+
+Variables can also be emptied by setting the value to NULL:
+
+
+
+*/
+
+$superVariable = "HELLO SAN FRANCISCO!";
+$superVariable = null;
+var_dump($superVariable);
+
+
+
+
 ?>
 
 </body>
