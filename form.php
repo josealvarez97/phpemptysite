c11385406132007be5dcd08a26397445569cc4fa
José Alvarez <cachorritoalfa2013@gmail.com>
2017-05-29 23:12:16 -0600

    Update form.php

--- form.php
+++ form.php
@@ -1,6 +1,7 @@
 <html>
 <body>
 
+<a href ="https://www.w3schools.com/php/php_forms.asp">Good introductory article, great actually from w3schools.</a>
 <form action="welcome.php" method="post">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
