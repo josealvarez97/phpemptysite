c0b723dd3a7579a2f23990b31a53470c3b5255d5
José Alvarez <cachorritoalfa2013@gmail.com>
2017-05-29 21:13:55 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,13 +10,13 @@ echo "Hello World! atte: " . $name . "<br>";
 
 function tryName() {
 	global $name;
-	echo "<p>Hello people, this a test: my name is $name " . $GLOBALS['lastName'] . "<\p>";
+	echo "<p>Hello people, this a test: my name is $name " . $GLOBALS['lastName'] . "</p>";
 }
 
 
 function IronManMovieCounter(){
 	static $movieNumber = 1;
-	echo "<p>Ironman " . $movieNumber . "<p>";
+	echo "<p>Ironman " . $movieNumber . "</p>";
 	$movieNumber++;
 }
 
