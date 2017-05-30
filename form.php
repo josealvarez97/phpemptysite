33b412e8137c7863cf1f52cb9d32206ef3294774
José Alvarez <cachorritoalfa2013@gmail.com>
2017-05-30 12:15:08 -0600

    Update form.php

--- form.php
+++ form.php
@@ -5,13 +5,42 @@
 
 // define variables and set to empty values
 $name = $email = $gender = $comment = $website = "";
+$nameErr = $emailErr = $genderErr = $commentErr = $websiteErr = "";
+
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$name = test_input($_POST["name"]);
-	$email = test_input($_POST["email"]);
-	$gender = test_input($_POST["gender"]);
-	$comment = test_input($_POST["comment"]);
-	$website = test_input($_POST["website"]);
+
+	if(empty($_POST["name"])) {
+		$nameErr = "Name is required";
+	} else {
+		$name = test_input($_POST["name"]);
+	}
+
+	if(empty($_POST["email"])) {
+		$emailErr = "Email is required";
+	} else {
+		$email = test_input($_POST["email"]);
+	}
+
+	if(empty($_POST["website"])) {
+		$website = "";
+	} else {
+		$website = test_input($_POST['website']);
+	}
+
+	if(empty($_POST['comment']) {
+		$comment = "";
+	} else {
+		$comment = test_input($_POST['comment']);
+	}
+
+	if(empty($_POST['gender'])) {
+		$gender = "Gender is required";
+	} else {
+		$gender = test_input($_POST['gender']);
+	}
+
+
 }
 
 function test_input($data) {
@@ -46,16 +75,22 @@ E-mail: <input type="text" name="email"><br>
 
 <h2>VALIDATING A FORM</h2>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+
 Name: <input type="text" name="name">
+<span class="error">* <?php echo $nameErr; ?></span>
 <br><br>
 E-mail: <input type="text" name="email">
+<span class="error">* <?php echo $emailErr; ?></span>
 <br><br>
 Website: <input type="text" name="website">
+<span class="error">* <?php echo $websiteErr; ?></span>
 <br><br>
 Comment: <textarea name="comment" rows="5" cols="40"></textarea>
 <br><br>
 Gender: <input type="radio" name="gender" value="female">Female
 <input type="radio" name="gender" value="male">Male
+<span class="error">* <?php echo $genderErr; ?></span>
 <br><br>
 <input type="submit" name="submit" value="Submit">
 </form>
