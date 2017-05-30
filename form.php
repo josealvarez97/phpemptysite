ee6d4d18f06f4dbb84093b84b13be068a904ec6d
José Alvarez <cachorritoalfa2013@gmail.com>
2017-05-30 11:44:06 -0600

    Update form.php

--- form.php
+++ form.php
@@ -1,6 +1,31 @@
 <html>
 <body>
 
+<?php
+
+// define variables and set to empty values
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$name = test_input($_POST["name"]);
+	$email = test_input($_POST["email"]);
+	$gender = test_input($_POST["gender"]);
+	$comment = test_input($_POST["comment"]);
+	$website = test_input($_POST["website"]);
+}
+
+function test_input($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+
+	return $data;
+}
+
+
+
+?>
+
 <a href ="https://www.w3schools.com/php/php_forms.asp">Good introductory article, great actually from w3schools.</a>
 
 <p>Form with post method</p>  
@@ -17,6 +42,26 @@ Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
 </form>
+
+
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+Name: <input type="text" name="name">
+E-mail: <input type="text" name="email">
+Website: <input type="text" name="website">
+Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+Gender: <input type="radio" name="gender" value="female">Female
+<input type="radio" name="gender" value="male">Male
+</form>
+
+
+
+
+
+
+
+
+
 
 </body>
 </html>
