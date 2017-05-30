a7b470026f275da64e1f780ce5fc4d266d7ef371
José Alvarez <cachorritoalfa2013@gmail.com>
2017-05-29 23:33:55 -0600

    Update form.php

--- form.php
+++ form.php
@@ -2,7 +2,17 @@
 <body>
 
 <a href ="https://www.w3schools.com/php/php_forms.asp">Good introductory article, great actually from w3schools.</a>
+
+<p>Form with post method</p>  
 <form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+  
+<p>Form with get method</p>  
+
+<form action="welcome_get.php" method="get">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
